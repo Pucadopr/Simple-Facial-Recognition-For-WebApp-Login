960fe28cb1b109ffdb98624cf51e6eabf064c3f1
notanewbiecoder <40542114+NotaNewbieCoder@users.noreply.github.com>
2020-05-15 04:37:04 +0100

    commit

--- app/static/webcam.js
+++ app/static/webcam.js
@@ -225,7 +225,7 @@
         attach: function(elem) {
             // create webcam preview and attach to DOM element
             // pass in actual DOM reference, ID, or CSS selector
-            if (typeof(elem) == 'string') {
+            if (typeof(elem) == 'content') {
                 elem = document.getElementById(elem) || document.querySelector(elem);
             }
             if (!elem) {
